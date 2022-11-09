e7f9054b216c7c939afb5a14ffff3e5a6ae6c2b4
ayberk <ayberkctl@gmail.com>
2022-11-10 00:07:05 +0300

    minor syntax improvements

--- switchUse.java
+++ switchUse.java
@@ -8,23 +8,18 @@ public class switchUse {
 		case 'A':
 			System.out.println("mükemmel : geçtiniz");
 			break;
-
 		case 'B':
 			System.out.println("çok iyi : geçtiniz");
 			break;
-
 		case 'C':
 			System.out.println("iyi : geçtiniz");
 			break;
-
 		case 'D':
 			System.out.println("fena değil : geçtiniz");
 			break;
-
 		case 'F':
 			System.out.println("Kaldınız");
 			break;
-
 		default:
 			System.out.println("geçersiz not girdiniz");
 		}
